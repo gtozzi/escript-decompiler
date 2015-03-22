c3e1429e24ea5d5894afae0e236e38cbfe09c56d
Gabriele Tozzi <gabriele@tozzi.eu>
2015-03-22 23:57:19 +0100

    Decompiler: adding license

--- scripts/decompile.py
+++ scripts/decompile.py
@@ -2,6 +2,24 @@
 
 '''
 EScript decompiler for binary ECL files version 2 (POL093)
+
+@author Bodom @ ZHI Time Warp <bodom@discosucks.it>, special thanks to SyZyGy
+        for all its great work and its advice
+
+@version 0.9 (about 97% done)
+
+@license This program is free software: you can redistribute it and/or modify
+         it under the terms of the GNU General Public License as published by
+         the Free Software Foundation, either version 3 of the License, or
+         (at your option) any later version.
+
+         This program is distributed in the hope that it will be useful,
+         but WITHOUT ANY WARRANTY; without even the implied warranty of
+         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+         GNU General Public License for more details.
+
+         You should have received a copy of the GNU General Public License
+         along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import os, sys
